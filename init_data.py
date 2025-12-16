15ebf0ddad9b8a3e1f48efa0057eda473f00e4c5
Yooby Gigandet <yooby@macbook-pro-de-yooby.home>
2025-12-16 09:43:25 +0100

    Only downloading the raw data from Kaggle

--- init_data.py
+++ init_data.py
@@ -1,6 +1,5 @@
 # pylint: disable=missing-module-docstring
 
-import duckdb
 import kagglehub
 import pandas as pd
 
@@ -12,14 +11,3 @@ data = pd.read_csv(f"{path}/amazon_delivery.csv")
 
 # Save data in the project
 data.to_csv("data/raw/amazon_delivery.csv", index=False)
-
-### Creation of the database for SQL queries.
-
-con = duckdb.connect(database="data/raw/amazon_delivery.duckdb", read_only=False)
-
-con.execute(
-    """
-    CREATE OR REPLACE TABLE amazon_delivery AS
-    SELECT * FROM read_csv_auto('data/raw/amazon_delivery.csv')
-"""
-)
