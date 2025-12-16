6b5ee592375ed3524a6af671fe3b45a052a0e1cb
Yooby Gigandet <yooby@macbook-pro-de-yooby.home>
2025-12-16 11:05:11 +0100

    Compute distance from the dataset

---
+++ analysis.py
@@ -0,0 +1,52 @@
+"""
+Compute and update straight-line distances for Amazon delivery dataset.
+"""
+
+import duckdb
+from geopy.distance import geodesic
+
+
+def compute_distance(lat1, lon1, lat2, lon2):
+    """
+    Compute geodesic distance in meters.
+    :return: distance in meters
+    """
+    return int(geodesic((lat1, lon1), (lat2, lon2)).meters)
+
+
+# https://api.openrouteservice.org/ for the API
+
+# First, I simulate that the data came from a database: amazon_delivery
+
+con = duckdb.connect(
+    database="data/processed/amazon_delivery_clean.duckdb", read_only=False
+)
+
+# Compute the 'straight' distance between two coordinates
+
+# First create a new column in the database
+
+con.execute("ALTER TABLE amazon_delivery ADD COLUMN IF NOT EXISTS distance INTEGER")
+
+# Compute the straight distance between coordinates, not ideal, but enough for now
+
+rows = con.execute(
+    """
+    SELECT Order_ID, Store_Latitude, Store_Longitude, Drop_Latitude, Drop_Longitude
+    FROM amazon_delivery
+    WHERE distance IS NULL
+"""
+).fetchall()
+
+for row in rows:
+    Order_ID, Store_Latitude, Store_Longitude, Drop_Latitude, Drop_Longitude = row
+    # Compute geodesic distance
+    d = compute_distance(Store_Latitude, Store_Longitude, Drop_Latitude, Drop_Longitude)
+    # Update DuckDB directly
+    con.execute(
+        "UPDATE amazon_delivery SET distance = ? WHERE Order_ID = ?", (d, Order_ID)
+    )
+
+# pylint: disable=fixme
+# TODO: Validate distances for last-mile delivery.
+# Typical threshold: 30 km (30,000 meters)
